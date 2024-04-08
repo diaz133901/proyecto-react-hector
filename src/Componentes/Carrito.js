8a2723c063453ad3cdd1b14bd5a5768183c9c3c0
diaz133901 <hectordiazre@gmail.com>
2024-04-08 23:49:03 +0200

    Cambio pequeño en el total del dinero del pedido

--- src/Componentes/Carrito.js
+++ src/Componentes/Carrito.js
@@ -56,11 +56,8 @@ const Carrito = ({ carrito, setCarrito, productos }) => {
           return null;
         })}
       </ul>
-      <p>Total: ${calcularPrecioTotal(carrito, productos)}</p>
-      <button
-        className="btn btn-primary" // Clases de Bootstrap para un botón azul
-        onClick={handleGuardarPedido}
-      >
+      <p>Total: {calcularPrecioTotal(carrito, productos)} € </p>
+      <button className="btn btn-primary" onClick={handleGuardarPedido}>
         Realizar Pedido
       </button>
     </div>
